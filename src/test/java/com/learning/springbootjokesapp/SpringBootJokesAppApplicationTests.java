09358599e8d898a75a8629ae523f94135e7a725a
Aditya Patwa <adityapatwa@live.com>
2020-05-01 10:50:34 -0400

    Initial commit

---
+++ src/test/java/com/learning/springbootjokesapp/SpringBootJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springbootjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
